95d28073666f143ef7580f233250592f6970a14c
Dat01 <thanhdatduong24102001@gmail.com>
2023-10-29 11:45:38 +0700

    chua fix duoc loi

--- src/main/java/com/practiceproject/EmployeeManagementSystem/EmployeeManagementSystemApplication.java
+++ src/main/java/com/practiceproject/EmployeeManagementSystem/EmployeeManagementSystemApplication.java
@@ -9,4 +9,8 @@ public class EmployeeManagementSystemApplication {
 		SpringApplication.run(EmployeeManagementSystemApplication.class, args);
 	}
 	//TODO: Nếu hiển thị được dữ liệu thì sẽ bổ sung thêm phần giới tính cho Nhân viên.
+	/*Vẫn lỗi không hiển thị được dữ liệu có thể là do lỗi liên kết hoặc có thể là do phần khai
+	 * báo thuộc tính dữ liệu của mình bị sai nhưng cần phải xem lại của nó hoặc có khi cứ thử 
+	 * tạo cách thêm dữ liệu trước vậy.
+	*/
 }
