092172ca693c9fc4df8069016321fdab6e0479c7
Dat01 <thanhdatduong24102001@gmail.com>
2023-10-31 00:32:01 +0700

    add message

--- src/main/java/com/practiceproject/EmployeeManagementSystem/EmployeeManagementSystemApplication.java
+++ src/main/java/com/practiceproject/EmployeeManagementSystem/EmployeeManagementSystemApplication.java
@@ -9,8 +9,11 @@ public class EmployeeManagementSystemApplication {
 		SpringApplication.run(EmployeeManagementSystemApplication.class, args);
 	}
 	//TODO: Nếu hiển thị được dữ liệu thì sẽ bổ sung thêm phần giới tính cho Nhân viên.
-	/*Đã chạy ổn được phần thêm dữ liệu nhân viên rồi giờ thì chỉ cần cố tìm xem dữ liệu đã
-	 * lưu ở đâu thôi vì chắc chắn nó không được lưu ở trên máy của mình và bổ xung thêm chức 
-	 * năng xóa
+	/* Có vẻ là có lỗi: 
+	 * Đầu tiên là về phần sửa nhân viên thì sau khi sửa xong nó sẽ gần như tạo ra 1 nhân viên mới chứ nó không
+	 * thay thế cái phần mình sửa
+	 * Tiếp theo là phần xóa thì có vẻ là nó chỉ xóa phần hiện ra thôi còn phần dữ liệu thì nó vẫn lưu ở trong 
+	 * CSDL nên sẽ cần phải xem và sửa lại
+	 * 
 	*/
 }
