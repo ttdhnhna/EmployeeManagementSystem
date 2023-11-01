fc6ca0dbfcf284c638056b426cc8c3a5712fbf4e
Dat01 <thanhdatduong24102001@gmail.com>
2023-11-01 11:22:12 +0700

    chay thu va ko loi

--- src/main/java/com/practiceproject/EmployeeManagementSystem/EmployeeManagementSystemApplication.java
+++ src/main/java/com/practiceproject/EmployeeManagementSystem/EmployeeManagementSystemApplication.java
@@ -8,11 +8,4 @@ public class EmployeeManagementSystemApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeManagementSystemApplication.class, args);
 	}
-	/* Có vẻ là có lỗi: 
-	 * Đầu tiên là về phần sửa nhân viên thì sau khi sửa xong nó sẽ gần như tạo ra 1 nhân viên mới chứ nó không
-	 * thay thế cái phần mình sửa
-	 * Tiếp theo là phần xóa thì có vẻ là nó chỉ xóa phần hiện ra thôi còn phần dữ liệu thì nó vẫn lưu ở trong 
-	 * CSDL nên sẽ cần phải xem và sửa lại
-	 * 
-	*/
 }
