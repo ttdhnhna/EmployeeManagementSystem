698c814f70310d0b3761b414250e79715e8daaaf
Dat02 <thanhdatduong24102001@gmail.com>
2023-11-08 17:39:55 +0700

    add mess

--- src/main/java/com/practiceproject/EmployeeManagementSystem/EmployeeManagementSystemApplication.java
+++ src/main/java/com/practiceproject/EmployeeManagementSystem/EmployeeManagementSystemApplication.java
@@ -8,6 +8,5 @@ public class EmployeeManagementSystemApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeManagementSystemApplication.class, args);
 	}
-	//TODO: Đầu tiên là làm một trang web để có thể hiển thị ra đầy đủ thông tin nhân viên.
-	//Đã chạy được các chức năng rồi giờ sẽ thêm nốt chức năng sửa phòng ban và tạo phần đăng nhập
+	//TODO: Thêm một branch khác để xử lý chức năng login
 }
