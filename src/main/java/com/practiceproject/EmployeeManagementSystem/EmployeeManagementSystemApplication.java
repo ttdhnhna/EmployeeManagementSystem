05fcf73ff81c1bf3a48b0c834372c3c330b29bb1
Dat02 <thanhdatduong24102001@gmail.com>
2023-11-26 17:38:25 +0700

    update login & logout functions

--- src/main/java/com/practiceproject/EmployeeManagementSystem/EmployeeManagementSystemApplication.java
+++ src/main/java/com/practiceproject/EmployeeManagementSystem/EmployeeManagementSystemApplication.java
@@ -7,5 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EmployeeManagementSystemApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeManagementSystemApplication.class, args);
+		//TODO: Thử chạy chức năng đăng nhập trước đã rồi xóa chú thích để thực hiện chức năng dẫn ra trang chủ sau.
 	}
 }
