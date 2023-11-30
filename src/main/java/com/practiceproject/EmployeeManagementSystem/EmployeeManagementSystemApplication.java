d44bbeeb9299cb2ef3e830234d892f7b1798a688
Dat02 <thanhdatduong24102001@gmail.com>
2023-11-30 17:34:29 +0700

    update fix errors

--- src/main/java/com/practiceproject/EmployeeManagementSystem/EmployeeManagementSystemApplication.java
+++ src/main/java/com/practiceproject/EmployeeManagementSystem/EmployeeManagementSystemApplication.java
@@ -8,5 +8,10 @@ public class EmployeeManagementSystemApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeManagementSystemApplication.class, args);
 		//TODO: Chạy thử chức năng đăng nhập
+		/*Chạy thử trước xem có vấn đề gì liên quan đến các dependencies security hay ko? Nếu
+		 * không thì xóa cái core đi còn nếu có thì thử thêm nó vào và làm tương tự với cái 
+		 * security web.
+		 * Xong rồi sẽ thêm cái tomcat vào xem có chạy đc chương trình ko 
+		 */
 	}
 }
