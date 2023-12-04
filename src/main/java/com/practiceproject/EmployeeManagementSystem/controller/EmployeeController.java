b669c742d6d926648c178e69306f9f7db746cbd4
Dat02 <thanhdatduong24102001@gmail.com>
2023-12-04 17:40:57 +0700

    update sorting function

--- src/main/java/com/practiceproject/EmployeeManagementSystem/controller/EmployeeController.java
+++ src/main/java/com/practiceproject/EmployeeManagementSystem/controller/EmployeeController.java
@@ -1,12 +1,16 @@
 package com.practiceproject.EmployeeManagementSystem.controller;
 
+// import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.practiceproject.EmployeeManagementSystem.entity.Employee;
 import com.practiceproject.EmployeeManagementSystem.service.EmployeeService;
@@ -57,5 +61,17 @@ public class EmployeeController {
     public String deleteEmployee(@PathVariable(value = "id") long id){
         this.service.deleteEmployeebyID(id);
         return "redirect:/";
+    }
+    @GetMapping("/")
+    public String findPaginated(@RequestParam("sortField") String sortField
+    , @RequestParam("sortDir") String sortDir
+    , Model model){
+        // Page<Employee> page=service.findPaginated(sortField, sortDir);
+        // List<Employee> listEmployees=page.getContent();
+
+        model.addAttribute("sortField", sortField);
+        model.addAttribute("sortDir", sortDir);
+        model.addAttribute("reverseSortDir", sortDir.equals("ask") ? "desc" : "asc");
+        return "homepage";
     }
 }
--- src/main/java/com/practiceproject/EmployeeManagementSystem/service/EmployeeService.java
+++ src/main/java/com/practiceproject/EmployeeManagementSystem/service/EmployeeService.java
@@ -3,6 +3,10 @@ package com.practiceproject.EmployeeManagementSystem.service;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.practiceproject.EmployeeManagementSystem.entity.Employee;
@@ -37,6 +41,11 @@ public class EmployeeService {
     public void deleteEmployeebyID(long id){
         this.repository.deleteById(id);
     }
-
+    public Page<Employee> findPaginated(String sortFeild, String sortDirection){
+        Sort sort=sortDirection.equalsIgnoreCase(Sort.Direction.ASC.name()) ? Sort.by(sortFeild).ascending() :
+        Sort.by(sortFeild).descending();
+        Pageable pageable=PageRequest.of(0, 0, sort);
+        return this.repository.findAll(pageable);
+    }
 }
 
