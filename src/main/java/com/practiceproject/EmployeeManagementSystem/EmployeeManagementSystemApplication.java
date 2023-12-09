b9899072113db3ae5e7ea13158bc89afc6038ef4
Dat02 <thanhdatduong24102001@gmail.com>
2023-12-09 17:25:06 +0700

    update sort function

--- src/main/java/com/practiceproject/EmployeeManagementSystem/EmployeeManagementSystemApplication.java
+++ src/main/java/com/practiceproject/EmployeeManagementSystem/EmployeeManagementSystemApplication.java
@@ -8,6 +8,5 @@ public class EmployeeManagementSystemApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeManagementSystemApplication.class, args);
 	}
-	//Đã chạy thành công được phần phân trang giờ thì tăng kích cỡ có thể lưu của 1 trang rồi làm phần sắp xếp.
-	
+	//TODO: Chạy thử trên laptop.
 }
