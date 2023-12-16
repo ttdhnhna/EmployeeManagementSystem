5741e8df757070ef5ae57f7226afd02f63b4d04a
Dat02 <thanhdatduong24102001@gmail.com>
2023-12-16 17:37:38 +0700

    update user profile page and homepage

--- src/main/java/com/practiceproject/EmployeeManagementSystem/repository/EmployeeRepository.java
+++ src/main/java/com/practiceproject/EmployeeManagementSystem/repository/EmployeeRepository.java
@@ -8,5 +8,5 @@ import com.practiceproject.EmployeeManagementSystem.entity.Employee;
 @Repository //@Repository được sử dụng để chỉ ra rằng lớp này cung cấp chức năng lưu trữ, truy xuất, tìm kiếm, cập nhật và xóa hoạt động trên các đối tượng
 public interface EmployeeRepository extends JpaRepository<Employee, Long>{
 //Lớp này được dùng để quản lý dữ liệu quan hệ trong các ứng dụng Java, cho phép ta truy cập và lưu trữ dữ liệu giữa các đối tượng Java và CSDL.
-    
+    @Query()
 }
