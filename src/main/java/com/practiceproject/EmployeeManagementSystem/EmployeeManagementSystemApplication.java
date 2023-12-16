2219d5afe678bf35e28ade4b06e42b952abd2929
Dat01 <thanhdatduong24102001@gmail.com>
2023-12-16 11:43:38 +0700

    success testing

--- src/main/java/com/practiceproject/EmployeeManagementSystem/EmployeeManagementSystemApplication.java
+++ src/main/java/com/practiceproject/EmployeeManagementSystem/EmployeeManagementSystemApplication.java
@@ -8,8 +8,9 @@ public class EmployeeManagementSystemApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeManagementSystemApplication.class, args);
 	}
-	/*Lỗi ở 2 phần sửa và chuyển trang của phòng ban còn đâu thì ở phần nhân viên
-	 * thì sắp xếp với chuyển trang đều ổn hết rồi.
+	/*Lỗi ở 2 phần sửa và chuyển trang của phòng ban 
 	 * Sửa lại 2 phần lỗi trên và làm phần trang chi tiết nhân viên.
+	 * Chạy thành công để vào phần hiện thông tin chi tiết nhân viên rồi và cả nút để quay lại
+	 * giờ sẽ làm hiện những thông tin ấy ra thôi
 	 */
 }
