bb63b20abd244d9706743bfbe8d8ade820dbe26e
Dat01 <thanhdatduong24102001@gmail.com>
2023-12-17 13:22:28 +0700

    test running success

--- src/main/java/com/practiceproject/EmployeeManagementSystem/EmployeeManagementSystemApplication.java
+++ src/main/java/com/practiceproject/EmployeeManagementSystem/EmployeeManagementSystemApplication.java
@@ -10,7 +10,7 @@ public class EmployeeManagementSystemApplication {
 	}
 	/*Lỗi ở 2 phần sửa và chuyển trang của phòng ban 
 	 * Sửa lại 2 phần lỗi trên và làm phần trang chi tiết nhân viên.
-	 * Chạy thành công để vào phần hiện thông tin chi tiết nhân viên rồi và cả nút để quay lại
-	 * giờ sẽ làm hiện những thông tin ấy ra thôi
+	 * Hiện thông tin thành công nhưng nên bổ sung thêm hiện id của nhân viên
+	 * ra luôn ở phần chung
 	 */
 }
