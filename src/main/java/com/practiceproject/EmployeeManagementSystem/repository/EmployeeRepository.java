56798d6c353b22adbc2c635afad933ba941ea892
Dat02 <thanhdatduong24102001@gmail.com>
2023-12-18 17:42:51 +0700

    add seach and reset function for all employee page

--- src/main/java/com/practiceproject/EmployeeManagementSystem/repository/EmployeeRepository.java
+++ src/main/java/com/practiceproject/EmployeeManagementSystem/repository/EmployeeRepository.java
@@ -11,8 +11,8 @@ import com.practiceproject.EmployeeManagementSystem.entity.Employee;
 @Repository //@Repository được sử dụng để chỉ ra rằng lớp này cung cấp chức năng lưu trữ, truy xuất, tìm kiếm, cập nhật và xóa hoạt động trên các đối tượng
 public interface EmployeeRepository extends JpaRepository<Employee, Long>{
 //Lớp này được dùng để quản lý dữ liệu quan hệ trong các ứng dụng Java, cho phép ta truy cập và lưu trữ dữ liệu giữa các đối tượng Java và CSDL.
-    @Query("SELECT p FROM Employee p WHERE p.hoten LIKE %?1%"
-    + "OR p.idnv LIKE %?1%"
-    + "OR p.chucvu LIKE %?1%")
+    @Query("SELECT p FROM Employee p WHERE"
+    + "CONCAT(p.idnv, p.hoten, p.chucvu)"
+    + "LIKE %?1%")
     public List<Employee> findAll(String keywords);
 }
