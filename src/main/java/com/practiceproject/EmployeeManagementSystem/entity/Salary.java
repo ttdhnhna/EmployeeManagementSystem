4ea2c7384117b807c5081c134d2698572f96b2af
Dat02 <thanhdatduong24102001@gmail.com>
2023-12-20 17:39:56 +0700

    add salary

---
+++ src/main/java/com/practiceproject/EmployeeManagementSystem/entity/Salary.java
@@ -0,0 +1,15 @@
+package com.practiceproject.EmployeeManagementSystem.entity;
+
+/**
+ * Salary
+ */
+public class Salary {
+    private Long idluong;
+    private Long idnv;
+    private double luongcb;
+    private double hsl;
+    private double phucap;
+    private double baohiem;
+    private double truluong;
+    /*Can suy nghi lai thuoc tinh de tinh so ngay di lam va nghi de con tru */
+}
--- src/main/java/com/practiceproject/EmployeeManagementSystem/repository/DepartmentRepository.java
+++ src/main/java/com/practiceproject/EmployeeManagementSystem/repository/DepartmentRepository.java
@@ -11,7 +11,8 @@ import com.practiceproject.EmployeeManagementSystem.entity.Department;
 @Repository
 public interface DepartmentRepository extends JpaRepository<Department, Long> {
     @Query("SELECT p FROM Department p WHERE p.tenpb LIKE %?1%"
+    + "OR p.idpb LIKE %?1%"
     + "OR p.diachi LIKE %?1%"
-    + "OR p.idpb LIKE %?1%")
+    + "OR p.sdt LIKE %?1%")
     public List<Department> findAllDepartments(String keyword);
 }
