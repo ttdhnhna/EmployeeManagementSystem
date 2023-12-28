ceac1f91e1196a17d8d605b94f5fad8f66fb6216
Dat01 <thanhdatduong24102001@gmail.com>
2023-12-28 21:55:04 +0700

    run test

--- src/main/java/com/practiceproject/EmployeeManagementSystem/EmployeeManagementSystemApplication.java
+++ src/main/java/com/practiceproject/EmployeeManagementSystem/EmployeeManagementSystemApplication.java
@@ -6,14 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EmployeeManagementSystemApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(EmployeeManagementSystemApplication.class, args);
-	}
-	/*Lỗi ở 2 phần sửa và chuyển trang của phòng ban 
-	 * Sửa lại 2 phần lỗi trên.
-	 * Phần tìm kiếm ở trang phòng ban vẫn chưa hoạt động đc nên sẽ để tạm đấy vậy
-	 * khi nào xử lý xong việc hiện danh sách nhân viên trong phòng ban thì sẽ bắt
-	 * đâu xử lý.
-	 * Đã làm xong phần hiện, thêm mã lương và liên kết giờ sẽ cần làm thêm lương với
-	 * hiện lương ở phần nhân viên là đc.
-	 */
+        SpringApplication.run(EmployeeManagementSystemApplication.class, args);
+    }
 }
