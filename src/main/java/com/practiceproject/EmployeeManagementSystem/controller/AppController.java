85bcd993897de5f96c534efc5b03267fc60d6995
Dat02 <thanhdatduong24102001@gmail.com>
2024-01-29 17:42:25 +0700

    update login func

--- src/main/java/com/practiceproject/EmployeeManagementSystem/controller/AppController.java
+++ src/main/java/com/practiceproject/EmployeeManagementSystem/controller/AppController.java
@@ -4,24 +4,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.practiceproject.EmployeeManagementSystem.entity.User;
-import com.practiceproject.EmployeeManagementSystem.service.AppService;
+import com.practiceproject.EmployeeManagementSystem.repository.UserRepository;
 
 @Controller
 public class AppController {
     @Autowired
-    AppService service;
+    UserRepository repository;
+
     @GetMapping("/registration")
-    public String regisPage(Model model){
-        model.addAttribute("user", new User());
+    public String registerPage(Model model){
+        model.addAttribute("user",new User());
         return "registration";
     }
     @PostMapping("/saveRegistration")
-    public String saveRegistration(@ModelAttribute("user") User user){
-        service.saveUser(user);
+    public String saveRegistration(User user){
+        this.repository.save(user);
         return "redirect:/";
     }
 }
