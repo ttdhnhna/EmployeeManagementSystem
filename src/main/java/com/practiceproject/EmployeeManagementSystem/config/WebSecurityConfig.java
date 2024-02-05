e3ed3097f1edad77996b12247d73df014fc60a48
Dat01 <thanhdatduong24102001@gmail.com>
2024-02-05 11:59:18 +0700

    run test

--- src/main/java/com/practiceproject/EmployeeManagementSystem/config/WebSecurityConfig.java
+++ src/main/java/com/practiceproject/EmployeeManagementSystem/config/WebSecurityConfig.java
@@ -48,7 +48,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-            .antMatchers("/registration")
+            .antMatchers("/registration**")
             .authenticated()
             .anyRequest().permitAll()
             .and()
