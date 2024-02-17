d4b331522d0924a5991c1ae52340f23f7184376f
Dat02 <thanhdatduong24102001@gmail.com>
2024-02-17 17:40:56 +0700

    update multiplelogin func

--- src/main/java/com/practiceproject/EmployeeManagementSystem/config/AdminSecurityConfig.java
+++ src/main/java/com/practiceproject/EmployeeManagementSystem/config/AdminSecurityConfig.java
@@ -1,48 +1,36 @@
 package com.practiceproject.EmployeeManagementSystem.config;
 
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.core.annotation.Order;
-// import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-// import org.springframework.security.core.userdetails.UserDetailsService;
-// import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-// import org.springframework.security.web.SecurityFilterChain;
-// import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-// import com.practiceproject.EmployeeManagementSystem.service.CustomUserDetailsService;
 
-// @Configuration
-// @Order(2)
+@Configuration
+@Order(2)
 public class AdminSecurityConfig {
-    // @Bean
-    // public UserDetailsService userDetailsService(){
-    //     return new CustomUserDetailsService();
-    // }
 
-    // @Bean
-    // public BCryptPasswordEncoder passwordEncoder(){
-    //     return new BCryptPasswordEncoder();
-    // }
-
-    // @Bean
-    // public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-    //     http.authorizeRequests().antMatchers("/")
-    //         .permitAll();
-    //     http.antMatcher("/loginadmin")
-    //         .authorizeRequests().anyRequest()
-    //         .hasAuthority("ADMIN")
-    //         .and()
-    //         .formLogin()
-    //             .loginPage("loginadmin")
-    //             .usernameParameter("email")
-    //             .permitAll()
-    //         .and()
-    //         .logout()
-    //             .invalidateHttpSession(true)
-    //             .clearAuthentication(true)
-    //             .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-    //             .logoutSuccessUrl("/login?logout")
-    //             .permitAll();
-    //     return http.build();
-    // }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.antMatcher("/loginadmin")
+            .authorizeRequests().anyRequest()
+            .hasAuthority("ADMIN")
+            .and()
+            .formLogin()
+                .loginPage("/loginadmin")
+                .usernameParameter("username")
+                .loginProcessingUrl("/loginadmin")
+                .defaultSuccessUrl("/")
+                .permitAll()
+            .and()
+            .logout()
+                .invalidateHttpSession(true)
+                .clearAuthentication(true)
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/loginadmin?logout")
+                .permitAll();
+        return http.build();
+    }
 }
--- src/main/java/com/practiceproject/EmployeeManagementSystem/config/UserSecurityConfig.java
+++ src/main/java/com/practiceproject/EmployeeManagementSystem/config/UserSecurityConfig.java
@@ -5,6 +5,7 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,13 +13,15 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import com.practiceproject.EmployeeManagementSystem.service.CustomUserDetailsService;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+@Order(1)
+public class UserSecurityConfig extends WebSecurityConfigurerAdapter{
     @Autowired
     DataSource dataSource;
 
@@ -45,15 +48,40 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
         auth.authenticationProvider(authenticationProvider());
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-            .antMatchers("/")//Hóa ra đây là chỗ yêu cầu cần đăng nhập mới có quyền truy cập. Câu lệnh này sẽ chỉnh đường dẫn đc thêm sẽ làm những gì. 
-            .authenticated()//Xác định danh tính người định truy cập đường link đc chỉ định hay là đường link ở trên.
-            .anyRequest().permitAll()
+    // @Override
+    // protected void configure(HttpSecurity http) throws Exception {
+    //     http.authorizeRequests()
+    //         .antMatchers("/")//Hóa ra đây là chỗ yêu cầu cần đăng nhập mới có quyền truy cập. Câu lệnh này sẽ chỉnh đường dẫn đc thêm sẽ làm những gì. 
+    //         .authenticated()//Xác định danh tính người định truy cập đường link đc chỉ định hay là đường link ở trên.
+    //         .anyRequest().permitAll()
+    //         .and()
+    //         .formLogin()
+    //             .loginPage("/login")
+    //             .permitAll()
+    //         .and()
+    //         .logout()
+    //             .invalidateHttpSession(true)
+    //             .clearAuthentication(true)
+    //             .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+    //             .logoutSuccessUrl("/login?logout")
+    //             .permitAll();
+    // }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.authorizeRequests().antMatchers("/")
+            .authenticated()
+            .anyRequest()
+            .permitAll();
+        http.antMatcher("/login")
+            .authorizeRequests().anyRequest()
+            .hasAuthority("USER")
             .and()
             .formLogin()
                 .loginPage("/login")
+                .usernameParameter("username")
+                .loginProcessingUrl("/login")
+                .defaultSuccessUrl("/userpage")
                 .permitAll()
             .and()
             .logout()
@@ -62,6 +90,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
                 .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
                 .logoutSuccessUrl("/login?logout")
                 .permitAll();
+        return http.build();
     }
 
     
