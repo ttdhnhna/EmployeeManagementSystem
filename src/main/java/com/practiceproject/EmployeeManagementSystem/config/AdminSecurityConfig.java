bde5f25455890cf923b9eacfd638877e9ba4ec67
Dat02 <thanhdatduong24102001@gmail.com>
2024-02-20 16:47:46 +0700

    fix loi

--- src/main/java/com/practiceproject/EmployeeManagementSystem/config/AdminSecurityConfig.java
+++ src/main/java/com/practiceproject/EmployeeManagementSystem/config/AdminSecurityConfig.java
@@ -1,37 +1,37 @@
 package com.practiceproject.EmployeeManagementSystem.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.core.annotation.Order;
+// import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+// import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+// import org.springframework.security.web.SecurityFilterChain;
+// import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 
-@Configuration
-@Order(2)
-public class AdminSecurityConfig extends WebSecurityConfigurerAdapter{
+// @Configuration
+// @Order(2)
+public class AdminSecurityConfig {
 
-    @Bean
-    public SecurityFilterChain filterChain2(HttpSecurity http) throws Exception{
-        http.antMatcher("/loginadmin")
-            .authorizeRequests().anyRequest()
-            .hasAuthority("ADMIN")
-            .and()
-            .formLogin()
-                .loginPage("/loginadmin")
-                .usernameParameter("username")
-                .loginProcessingUrl("/loginadmin")
-                .defaultSuccessUrl("/")
-                .permitAll()
-            .and()
-            .logout()
-                .invalidateHttpSession(true)
-                .clearAuthentication(true)
-                .logoutRequestMatcher(new AntPathRequestMatcher("/logoutadmin"))
-                .logoutSuccessUrl("/loginadmin?logout")
-                .permitAll();
-        return http.build();
-    }
+    // @Bean
+    // public SecurityFilterChain filterChain2(HttpSecurity http) throws Exception{
+    //     http.antMatcher("/loginadmin")
+    //         .authorizeRequests().anyRequest()
+    //         .hasAuthority("ADMIN")
+    //         .and()
+    //         .formLogin()
+    //             .loginPage("/loginadmin")
+    //             .usernameParameter("username")
+    //             .loginProcessingUrl("/loginadmin")
+    //             .defaultSuccessUrl("/")
+    //             .permitAll()
+    //         .and()
+    //         .logout()
+    //             .invalidateHttpSession(true)
+    //             .clearAuthentication(true)
+    //             .logoutRequestMatcher(new AntPathRequestMatcher("/logoutadmin"))
+    //             .logoutSuccessUrl("/loginadmin?logout")
+    //             .permitAll();
+    //     return http.build();
+    // }
 }
---
+++ src/main/java/com/practiceproject/EmployeeManagementSystem/config/SecurityConfig.java
@@ -0,0 +1,76 @@
+package com.practiceproject.EmployeeManagementSystem.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import com.practiceproject.EmployeeManagementSystem.service.CustomUserDetailsService;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    DataSource dataSource;
+
+    @Autowired
+    private CustomUserDetailsService customUserDetailsService;
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(authenticationProvider());
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(customUserDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/login").hasAuthority("USER")
+                .antMatchers("/loginadmin").hasAuthority("ADMIN")
+                .antMatchers("/").authenticated()
+                .anyRequest().permitAll()
+                .and()
+            .formLogin()
+                .loginPage("/login")
+                .loginProcessingUrl("/login")
+                .defaultSuccessUrl("/userpage")
+                .usernameParameter("username")
+                .permitAll()
+                .and()
+            .formLogin()
+                .loginPage("/loginadmin")
+                .loginProcessingUrl("/loginadmin")
+                .defaultSuccessUrl("/")
+                .usernameParameter("username")
+                .permitAll()
+                .and()
+            .logout()
+                .invalidateHttpSession(true)
+                .clearAuthentication(true)
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/login?logout")
+                .permitAll();
+    }
+}
+
