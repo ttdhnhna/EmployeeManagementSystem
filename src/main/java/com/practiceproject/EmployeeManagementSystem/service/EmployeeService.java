c485733925f937d6324f7a43126a13407ca903a5
Dat02 <thanhdatduong24102001@gmail.com>
2024-03-31 15:20:07 +0700

    fix loi

--- src/main/java/com/practiceproject/EmployeeManagementSystem/service/EmployeeService.java
+++ src/main/java/com/practiceproject/EmployeeManagementSystem/service/EmployeeService.java
@@ -38,7 +38,7 @@ public class EmployeeService {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        repository.save(employee);
+        this.repository.save(employee);
     }
     //Tìm nhân viên bằng id
     public Employee getEmployeebyID(long id){
