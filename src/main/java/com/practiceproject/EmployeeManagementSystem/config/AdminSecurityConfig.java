b4bb7ab7e02f8ad8e0edb381a495fa20a50712ae
Dat02 <thanhdatduong24102001@gmail.com>
2024-04-07 17:27:54 +0700

    fix loi

--- src/main/java/com/practiceproject/EmployeeManagementSystem/config/AdminSecurityConfig.java
+++ src/main/java/com/practiceproject/EmployeeManagementSystem/config/AdminSecurityConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-// import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -41,10 +41,10 @@ public class AdminSecurityConfig extends WebSecurityConfigurerAdapter{
         return authProvider;
     }
 
-    // @Override
-    // protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-    //     auth.authenticationProvider(authenticationProvider());
-    // }
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(authenticationProvider());
+    }
     
     @Override 
     protected void configure(HttpSecurity http) throws Exception{
