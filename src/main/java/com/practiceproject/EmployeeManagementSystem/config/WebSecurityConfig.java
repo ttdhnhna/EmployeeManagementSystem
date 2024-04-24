e278d4a0da65634bb63e4286f9328bf8d1f860f8
Dat02 <thanhdatduong24102001@gmail.com>
2024-04-24 17:16:36 +0700

    xoa login de chay thu

--- src/main/java/com/practiceproject/EmployeeManagementSystem/config/WebSecurityConfig.java
+++ src/main/java/com/practiceproject/EmployeeManagementSystem/config/WebSecurityConfig.java
@@ -4,11 +4,11 @@ import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+// import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+// import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -16,8 +16,8 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import com.practiceproject.EmployeeManagementSystem.service.CustomUserDetailsService;
 
-@Configuration
-@EnableWebSecurity
+// @Configuration
+// @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
     @Autowired
     DataSource dataSource;
