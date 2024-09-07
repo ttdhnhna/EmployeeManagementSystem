50390ae6299e073f1352f092d37909fbce1e4a78
Dat01 <thanhdatduong24102001@gmail.com>
2024-09-07 16:59:54 +0700

    run test

--- src/main/java/com/practiceproject/EmployeeManagementSystem/EmployeeManagementSystemApplication.java
+++ src/main/java/com/practiceproject/EmployeeManagementSystem/EmployeeManagementSystemApplication.java
@@ -7,7 +7,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 public class EmployeeManagementSystemApplication {
-	public static void main(String[] args) {
+	public static void main(String[] args)  {
         SpringApplication.run(EmployeeManagementSystemApplication.class, args);
     }
 }
