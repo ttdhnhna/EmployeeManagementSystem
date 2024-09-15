64ec19622bcf05a419f7b80429ba1a1d1b4a3af3
Dat02 <thanhdatduong24102001@gmail.com>
2024-09-15 17:33:04 +0700

    fix loi

--- src/main/java/com/practiceproject/EmployeeManagementSystem/config/WebConfig.java
+++ src/main/java/com/practiceproject/EmployeeManagementSystem/config/WebConfig.java
@@ -1,7 +1,9 @@
 package com.practiceproject.EmployeeManagementSystem.config;
 
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
@@ -20,4 +22,13 @@ public class WebConfig implements WebMvcConfigurer{
     public void addInterceptors(InterceptorRegistry registry){
         registry.addInterceptor(localeChangeInterceptor());
     }
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:messages"); 
+        messageSource.setDefaultEncoding("UTF-8");       
+        messageSource.setCacheSeconds(0);                
+        return messageSource;
+}
 }
