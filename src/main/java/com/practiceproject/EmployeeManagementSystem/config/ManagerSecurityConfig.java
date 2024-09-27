f42b7a07cce353c35da6c56f4f586001c949efc9
Dat02 <thanhdatduong24102001@gmail.com>
2024-09-27 20:47:20 +0700

    fix loi

--- src/main/java/com/practiceproject/EmployeeManagementSystem/config/ManagerSecurityConfig.java
+++ src/main/java/com/practiceproject/EmployeeManagementSystem/config/ManagerSecurityConfig.java
@@ -3,8 +3,8 @@ package com.practiceproject.EmployeeManagementSystem.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 
-@Configuration
-@Order(1)
+// @Configuration
+// @Order(1)
 public class ManagerSecurityConfig {
     
 }
