88d10d9e0411fc5b5a0b2d7f64dfa240533df938
Dat02 <thanhdatduong24102001@gmail.com>
2024-09-29 21:19:06 +0700

    fix loi dang nhap

--- src/main/java/com/practiceproject/EmployeeManagementSystem/config/ManagerSecurityConfig.java
+++ src/main/java/com/practiceproject/EmployeeManagementSystem/config/ManagerSecurityConfig.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -31,6 +33,25 @@ public class ManagerSecurityConfig extends WebSecurityConfigurerAdapter{
     @Bean
     public BCryptPasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider authProvider=new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+    // Purpose: The DaoAuthenticationProvider is a specific implementation of AuthenticationProvider that uses a data-access object (DAO) to retrieve user information.
+    // What it does: This provider takes care of fetching the user details (via UserDetailsService) and validating the password (using BCryptPasswordEncoder).
+    // It sets the UserDetailsService (from userDetailsService()) to fetch user details (like username, password, and roles).
+    // It sets the PasswordEncoder (from passwordEncoder()) to validate and compare the password.
+    // Why it's needed: This is the part of the authentication mechanism where the user’s credentials are verified. It compares the provided credentials (username and password) 
+    // with the stored ones (loaded by the CustomUserDetailsService and checked using BCryptPasswordEncoder).
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(authenticationProvider());
     }
 
     @Override
