599e8674e157f28316f0f075b378a1a866192491
Dat02 <thanhdatduong24102001@gmail.com>
2024-09-30 16:04:11 +0700

    fix loi dang nhap

--- src/main/java/com/practiceproject/EmployeeManagementSystem/config/EmployeeSecurityConfig.java
+++ src/main/java/com/practiceproject/EmployeeManagementSystem/config/EmployeeSecurityConfig.java
@@ -6,12 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-
 @Configuration
 @Order(2)
+@EnableWebSecurity
 public class EmployeeSecurityConfig extends WebSecurityConfigurerAdapter{
     @Autowired
     DataSource dataSource;
--- src/main/java/com/practiceproject/EmployeeManagementSystem/config/ManagerSecurityConfig.java
+++ src/main/java/com/practiceproject/EmployeeManagementSystem/config/ManagerSecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -19,9 +20,11 @@ import com.practiceproject.EmployeeManagementSystem.service.CustomUserDetailsSer
 
 @Configuration
 @Order(1)
+@EnableWebSecurity
 public class ManagerSecurityConfig extends WebSecurityConfigurerAdapter{
     @Autowired
     DataSource dataSource;
+
     @Autowired
     private CustomLogoutSuccessHandler logout;
 
@@ -57,7 +60,7 @@ public class ManagerSecurityConfig extends WebSecurityConfigurerAdapter{
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-            .antMatchers("/").hasAuthority("MANAGER")
+            .antMatchers("/").authenticated()
             .antMatchers("/manager/**").hasAuthority("MANAGER")
             .antMatchers("/registration", "/styles/**", "/image/**", "/forgotpassword"
                 , "/employee/login", "/saveRegistration", "/upforgotpassword", "/resetpassword"
