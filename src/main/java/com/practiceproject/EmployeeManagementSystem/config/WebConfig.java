2920c367c2757e0be6038086abbbbfdfd7704c42
Dat01 <thanhdatduong24102001@gmail.com>
2024-12-04 15:48:52 +0700

    update local

--- src/main/java/com/practiceproject/EmployeeManagementSystem/config/WebConfig.java
+++ src/main/java/com/practiceproject/EmployeeManagementSystem/config/WebConfig.java
@@ -19,7 +19,7 @@ public class WebConfig implements WebMvcConfigurer{
     }
 
     @Override
-    public void addInterceptors(InterceptorRegistry registry){
+    public void addInterceptors(@SuppressWarnings("null") InterceptorRegistry registry){
         registry.addInterceptor(localeChangeInterceptor());
     }
 
