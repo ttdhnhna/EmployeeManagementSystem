8df8d016047793755be4038dc8876887c01e26db
Dat01 <thanhdatduong24102001@gmail.com>
2024-12-06 16:00:53 +0700

    fix xong phan customsuccesshandler, chua lam gi dc o phan security config

---
+++ src/main/java/com/practiceproject/EmployeeManagementSystem/service/CustomSuccessHandler.java
@@ -0,0 +1,28 @@
+package com.practiceproject.EmployeeManagementSystem.service;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+
+public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler{
+
+    @Override
+    protected void handle(HttpServletRequest request, HttpServletResponse response, Authentication authentication)
+            throws IOException, ServletException {
+        String redirectUrl = authentication.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .filter(role -> role.equals("MANAGER") || role.equals("EMPLOYEE"))
+                .findFirst()
+                .map(role -> role.equals("MANAGER") ? "/" : "/employee/home")
+                .orElse("/login?error");
+        getRedirectStrategy().sendRedirect(request, response, redirectUrl);
+    }
+
+    
+}
