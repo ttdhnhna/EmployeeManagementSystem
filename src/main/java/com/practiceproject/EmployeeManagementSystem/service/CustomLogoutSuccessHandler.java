f9ceb6b4204aaf54fb38a861de825c54f3b83735
Dat01 <thanhdatduong24102001@gmail.com>
2025-01-03 16:12:35 +0700

    fix giao dien trang chu nv

--- src/main/java/com/practiceproject/EmployeeManagementSystem/service/CustomLogoutSuccessHandler.java
+++ src/main/java/com/practiceproject/EmployeeManagementSystem/service/CustomLogoutSuccessHandler.java
@@ -26,9 +26,9 @@ public class CustomLogoutSuccessHandler implements LogoutSuccessHandler{
             throws IOException, ServletException {
         String lang = request.getParameter("lang");
         if(authentication!=null && authentication.getName()!=null){
-            User user = uService.getAccountByEmail(authentication.getName());
+            // User user = uService.getAccountByEmail(authentication.getName());
 
-            eService.logAuditOperation(user, null, null, null, Act.LOGOUT);
+            // eService.logAuditOperation(user, null, null, null, Act.LOGOUT);
         }
         if (lang == null || (!lang.equals("en") && !lang.equals("vi"))) {
             lang = "en"; 
